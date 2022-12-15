d220d2068aaa3fc7b0e55c9a82d23268c5c520a9
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-15 20:19:10 +0100

    Update App.jsx
    Routes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import NavBar from './component/NavBar'
 import Home from './routes/Home'
+import Experiences from './routes/Experiences'
+import Trainning from './routes/Trainning'
+import HardSkills from './routes/HardSkills'
+import SoftSkills from './routes/SoftSkills'
+import Hobbies from './routes/Hobbies'
 
 export default function App() {
 
   return (
     <BrowserRouter>
+    <NavBar />
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="home" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/experiences" element={<Experiences />} />
+          <Route path="/trainning" element={<Trainning />} />
+          <Route path="/hardskills" element={<HardSkills />} />
+          <Route path="/softskills" element={<SoftSkills />} />
+          <Route path="/hobbies" element={<Hobbies />} />
         </Routes>
     </BrowserRouter>
   )
