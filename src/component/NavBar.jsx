2d2f5c009e453980c8dedb201283080bfed56be2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-15 20:19:39 +0100

    Update NavBar.jsx
    Links added.

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>Hello world NavBar !</div>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/experiences">Experiences</Link>
+      <Link to="/trainning">Trainning</Link>
+      <Link to="/hardskills">Hard Skills</Link>
+      <Link to="/softskills">Soft Skills</Link>
+      <Link to="/hobbies">Hobbies</Link>
+    </div>
   )
 }
