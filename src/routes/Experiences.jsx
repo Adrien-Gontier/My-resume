b0a759063397b075f54c9296959dd84bd07a1b70
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-17 11:00:03 +0100

    Update Experiences.jsx
    TODO added.

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+// Create a beautyful json file on github to cible and call with axios
+
 export default function Experiences() {
   return (
     <div>Hello world Experiences !</div>
