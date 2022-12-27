bdb51b2937d9b418fb6083c03175dd015a26eb04
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-27 11:26:26 +0100

    Update NavBar.jsx
    ClassName added and import Link fixed.

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
   return (
-    <div>
+    <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/experiences">Experiences</Link>
       <Link to="/trainning">Trainning</Link>
