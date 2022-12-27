0273548f1dd3b5b7c1f87d2ecef585e5448d8fa4
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-27 11:27:23 +0100

    Update Experiences.jsx
    Import OneExperience component added and tested.

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import OneExperience from './oneExperience'
 
 // Create a beautyful json file on github to cible and call with axios
 
 export default function Experiences() {
   return (
-    <div>Hello world Experiences !</div>
+    <>
+      <div>Hello world Experiences !</div>
+      <OneExperience />
+    </>
   )
 }
