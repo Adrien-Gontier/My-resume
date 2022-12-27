7c3085fbe4925ed7345ebaa95b29674c1afc05de
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-27 11:27:44 +0100

    Update HardSkills.jsx
    Import OneHardSkill component added and tested.

--- src/routes/HardSkills.jsx
+++ src/routes/HardSkills.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import OneHardSkill from './OneHardSkill'
 
 export default function HardSkills() {
   return (
-    <div>Hello world HardSkills !</div>
+    <>
+      <div>Hello world HardSkills !</div>
+      <OneHardSkill />
+    </>
   )
 }
