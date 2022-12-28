7010962a6d60b52a1c1969b5082d242f7af812f2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-12-28 09:52:41 +0100

    Update App.jsx
    Imports fixed and new routes languages added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
-import NavBar from './component/NavBar'
+import NavBar from './components/NavBar'
 import Home from './routes/Home'
 import Experiences from './routes/Experiences'
 import Trainning from './routes/Trainning'
 import HardSkills from './routes/HardSkills'
 import SoftSkills from './routes/SoftSkills'
 import Hobbies from './routes/Hobbies'
-import Title from './component/Title'
+import Title from './components/Title'
+import Languages from './routes/Languages'
 
 export default function App() {
 
@@ -23,6 +24,7 @@ export default function App() {
           <Route path="/trainning" element={<Trainning />} />
           <Route path="/hardskills" element={<HardSkills />} />
           <Route path="/softskills" element={<SoftSkills />} />
+          <Route path="/languages" element={<Languages />} />
           <Route path="/hobbies" element={<Hobbies />} />
         </Routes>
     </BrowserRouter>
