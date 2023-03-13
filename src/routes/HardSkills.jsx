0db1223dee4e0830d2a2fed20e3e10f405fc7f9b
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-13 11:55:49 +0100

    Update HardSkills.jsx
    Indentation improved

--- src/routes/HardSkills.jsx
+++ src/routes/HardSkills.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import OneHardSkill from '../components/OneHardSkill'
 
 export default function HardSkills() {
-  return (
-    <>
-      <div>Hello world HardSkills !</div>
-      <OneHardSkill />
-    </>
-  )
+    return (
+        <>
+            <div>Hello world HardSkills !</div>
+            <OneHardSkill />
+        </>
+    )
 }
