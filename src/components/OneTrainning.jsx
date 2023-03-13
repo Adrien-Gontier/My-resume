814b82f3057fd8326bba0aa6d0b6f1023a5504a7
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-13 11:56:17 +0100

    Update OneTrainning.jsx
    Indentation improved

--- src/components/OneTrainning.jsx
+++ src/components/OneTrainning.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function OneTrainning() {
-  return (
-    <div>Hello world OneTrainning !</div>
-  )
+    return <div>Hello world OneTrainning !</div>
 }
