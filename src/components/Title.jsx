6b8590e6d050a7a1c5bef550ba4d6b10149d1f0f
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-13 11:56:13 +0100

    Update Title.jsx
    Indentation improved

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
 export default function Title() {
-  return (
-    <p className="application-title">My resume</p>
-  )
+    return <p className="application-title">My resume</p>
 }
