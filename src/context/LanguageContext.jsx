a06314e1b6d954383ca3d1fa746b7037117a875d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-13 11:56:09 +0100

    Update LanguageContext.jsx
    Indentation improved

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -1,3 +1,3 @@
-import { createContext } from "react";
-const LanguageContext = createContext("english");
-export default LanguageContext;
+import { createContext } from 'react'
+const LanguageContext = createContext('english')
+export default LanguageContext
