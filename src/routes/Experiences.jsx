2cd4260eb6db85b3aac46dbc9a99a97ef9102f1a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-14 12:14:30 +0100

    Update Experiences.jsx
    Datas experience imported

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import OneExperience from '../components/OneExperience'
+import AllExperiences from '../assets/datas/experiences_ENG.json'
+import PageTitle from '../components/PageTitle'
 
 export default function Experiences() {
+    let test2 = []
+    for (let i = AllExperiences.length - 1; i >= 0; i--) {
+        test2.push(AllExperiences[i])
+    }
     return (
         <>
-            <div>Hello world Experiences !</div>
-            <OneExperience />
+            <PageTitle props="Expriences" />
+            {test2.map((eachExperience) => (
+                <OneExperience key={eachExperience.id} props={eachExperience} />
+            ))}
         </>
     )
 }
