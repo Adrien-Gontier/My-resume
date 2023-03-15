c5e126a6fee53c405b3910a1b2e14d2a8e70f6d7
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-15 16:50:20 +0100

    Update Experiences.jsx
    Variable name changed and name of import too

--- src/routes/Experiences.jsx
+++ src/routes/Experiences.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import OneExperience from '../components/OneExperience'
-import AllExperiences from '../assets/datas/experiences_ENG.json'
+import EachExperience from '../components/EachExperience'
+import AllExperiencesData from '../assets/datas/experiences_ENG.json'
 import PageTitle from '../components/PageTitle'
 
 export default function Experiences() {
-    let test2 = []
-    for (let i = AllExperiences.length - 1; i >= 0; i--) {
-        test2.push(AllExperiences[i])
+    let experiencesData = []
+    for (let i = AllExperiencesData.length - 1; i >= 0; i--) {
+        experiencesData.push(AllExperiencesData[i])
     }
     return (
         <>
             <PageTitle props="Expriences" />
-            {test2.map((eachExperience) => (
-                <OneExperience key={eachExperience.id} props={eachExperience} />
+            {experiencesData.map((eachExperience) => (
+                <EachExperience
+                    key={eachExperience.id}
+                    props={eachExperience}
+                />
             ))}
         </>
     )
