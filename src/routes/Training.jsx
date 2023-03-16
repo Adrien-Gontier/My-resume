34822c8f0bc0ecf164208a1da4a1a57681f9d54c
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-16 10:06:06 +0100

    Create Training.jsx
    Name file fixed

---
+++ src/routes/Training.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import EachTraining from '../components/EachTraining'
+
+export default function Training() {
+    return (
+        <>
+            <div>Hello world Training !</div>
+            <EachTraining />
+        </>
+    )
+}
