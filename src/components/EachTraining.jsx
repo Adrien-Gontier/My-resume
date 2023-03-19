166b2588bdc4516407c1f3af129d0bbe9cf75c0e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-19 17:05:32 +0100

    Update EachTraining.jsx
    New content for component

--- src/components/EachTraining.jsx
+++ src/components/EachTraining.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function EachTraining() {
-    return <div>EachTraining</div>
+export default function EachTraining({ props }) {
+    console.log(props)
+    const name = <p>{props.name}</p>
+    const grantedYear = <p>{props.grantedYear}</p>
+    const speciality = <p>{props.grantedYear}</p>
+    return (
+        <Link>
+            <div>
+                {name}
+                {grantedYear}
+                {speciality}
+            </div>
+        </Link>
+    )
 }
