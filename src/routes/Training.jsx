4201ae96a394b87c3628979edf336975285838fb
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-19 17:05:53 +0100

    Update Training.jsx
    Content for training page coded

--- src/routes/Training.jsx
+++ src/routes/Training.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import EachTraining from '../components/EachTraining'
+import AllTrainingData from '../assets/datas/training_ENG.json'
+import PageTitle from '../components/PageTitle'
 
 export default function Training() {
+    let trainingData = []
+    for (let i = AllTrainingData.length - 1; i >= 0; i--) {
+        trainingData.push(AllTrainingData[i])
+    }
     return (
         <>
-            <div>Hello world Training !</div>
-            <EachTraining />
+            <PageTitle props="Traning" />
+            {trainingData.map((eachTraining) => (
+                <EachTraining key={eachTraining.id} props={eachTraining} />
+            ))}
         </>
     )
 }
