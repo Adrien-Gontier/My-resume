d10a3f795277415407bea192fcddd3939fd17e84
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-20 20:09:30 +0100

    Update EachTraining.jsx
    New display data added

--- src/components/EachTraining.jsx
+++ src/components/EachTraining.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function EachTraining({ props }) {
-    console.log(props)
     const name = <p>{props.name}</p>
     const grantedYear = <p>{props.grantedYear}</p>
-    const speciality = <p>{props.grantedYear}</p>
+    const speciality = <p>{props.speciality}</p>
     return (
-        <Link>
+        <Link className='eachTraining'>
             <div>
                 {name}
                 {grantedYear}
