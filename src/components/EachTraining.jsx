94732ffc7617933ffae64996604d0ec247e2259d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-21 22:00:41 +0100

    Update EachTraining.jsx
    Tag fixed for name of training

--- src/components/EachTraining.jsx
+++ src/components/EachTraining.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function EachTraining({ props }) {
-    const name = <p>{props.name}</p>
+    const name = <h3>{props.name}</h3>
     const grantedYear = <p>{props.grantedYear}</p>
     const speciality = <p>{props.speciality}</p>
     return (
-        <Link className='eachTraining'>
+        <Link className="eachTraining">
             <div>
                 {name}
-                {grantedYear}
                 {speciality}
             </div>
+            <div>{grantedYear}</div>
         </Link>
     )
 }
