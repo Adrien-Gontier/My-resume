394c47edc6a8b06a4a8358a16a5509219bb7419a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-25 11:25:19 +0100

    Update Home.jsx
    component to display page title added

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import PageTitle from '../components/PageTitle'
 
 export default function Home() {
-    return <div className="home"></div>
+    return (
+        <div>
+            <PageTitle props="Home" />
+        </div>
+    )
 }
