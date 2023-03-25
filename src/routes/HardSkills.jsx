0686a407a0b7687130131c98044065596a1a92b9
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-25 11:25:12 +0100

    Update HardSkills.jsx
    component to display page title added

--- src/routes/HardSkills.jsx
+++ src/routes/HardSkills.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import EachHardSkill from '../components/EachHardSkill'
+import PageTitle from '../components/PageTitle'
 
 export default function HardSkills() {
     return (
         <>
+            <PageTitle props="Hard Skills" />
             <div>Hello world HardSkills !</div>
             <EachHardSkill />
         </>
