d4cd5c219a2264dd9fae7e5b854cbde6587bf819
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-29 11:44:10 +0200

    Update App.jsx
    Path in import update 'routes' to 'pages'.
    
    New route added for OneTraining page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './assets/sass/App.css'
 import NavBar from './components/NavBar'
-import Home from './routes/Home'
-import Experiences from './routes/Experiences'
-import Training from './routes/Training'
-import HardSkills from './routes/HardSkills'
-import SoftSkills from './routes/SoftSkills'
-import Hobbies from './routes/Hobbies'
+import Home from './pages/Home'
+import Experiences from './pages/Experiences'
+import Training from './pages/Training'
+import HardSkills from './pages/HardSkills'
+import SoftSkills from './pages/SoftSkills'
+import Hobbies from './pages/Hobbies'
 import Title from './components/Title'
-import Languages from './routes/Languages'
+import Languages from './pages/Languages'
 import LanguageContext from './context/LanguageContext'
-import OneExperience from './routes/OneExperience'
+import OneExperience from './pages/OneExperience'
+import OneTraining from './pages/OneTrainning'
 
 export default function App() {
     const [language, setLanguage] = useState('english')
@@ -30,6 +31,7 @@ export default function App() {
                         element={<OneExperience />}
                     />
                     <Route path="/training" element={<Training />} />
+                    <Route path="/onetraining/:id" element={<OneTraining />} />
                     <Route path="/hardskills" element={<HardSkills />} />
                     <Route path="/softskills" element={<SoftSkills />} />
                     <Route path="/languages" element={<Languages />} />
