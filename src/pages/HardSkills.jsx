37a5c2f14f25e3e3419c79082c2f3db120339c2b
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-29 11:49:03 +0200

    Create HardSkills.jsx
    name directory changed to 'pages' instead of 'routes'.

---
+++ src/pages/HardSkills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EachHardSkill from '../components/EachHardSkill'
+import PageTitle from '../components/PageTitle'
+
+export default function HardSkills() {
+    return (
+        <>
+            <PageTitle props="Hard Skills" />
+            <div>Hello world HardSkills !</div>
+            <EachHardSkill />
+        </>
+    )
+}
