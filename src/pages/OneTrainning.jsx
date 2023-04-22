8b999d7df18e82571389b7f705bf4fcd309556ed
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-22 11:49:43 +0200

    Update OneTrainning.jsx
    Coding continued

--- src/pages/OneTrainning.jsx
+++ src/pages/OneTrainning.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import PageTitle from '../components/PageTitle'
+import AllTrainning from '../assets/datas/trainning_ENG.json'
+import { useParams } from 'react-router-dom'
 
 export default function OneTraining() {
+    const { id } = useParams()
+    const oneTrainningData = AllTrainning.find((element) => element.id == id)
+    const name = <h3>{oneTrainningData.name}</h3>
+    const grantedYear = <p>{oneTrainningData.grantedYear}</p>
+    const speciality = <p>{oneTrainningData.speciality}</p>
     return (
         <>
             <PageTitle props="One Training" />
-            <div>
-                
+            <div className="oneTrainning">
+                {name}
+                {speciality}
+                {grantedYear}
             </div>
         </>
     )
